4625cfbbe9b8fa6bfb2ad7c12bb7e021fe271c25
eric_huang <godhand4826@gmail.com>
2021-09-28 16:27:33 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(()=>{
+    getJSON("/me").then(setUser).catch(()=>{});
+  },[])
+  if (!user) {
+    return <Login setUser={setUser}></Login>;
+  } else {
+    return <>
+      <Logout user={user} setUser={setUser}></Logout>
+    </>;
+  }
+}
+
+function Logout(props) {
+  const logout = () =>
+    postJSON("/logout").then(() => props.setUser(null)).catch(alert);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>hello, {props.user.name}</div>
+      <button onClick={() => logout()}>logout</button>
+    </>
+  );
+}
+
+function Login(props) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const register = () =>
+    postJSON("/register", { name, password }).then(props.setUser).catch(alert);
+  const login = () =>
+    postJSON("/login", { name, password }).then(props.setUser).catch(alert);
+  return (
+    <>
+      <label>name</label>
+      <input onKeyUp={(e) => setName(e.target.value)}></input>
+      <label>password</label>
+      <input onKeyUp={(e) => setPassword(e.target.value)}></input>
+      <button onClick={register}>register</button>
+      <button onClick={login}>login</button>
+    </>
   );
 }
 
 export default App;
+
+const baseURL = "http://localhost:9999";
+async function postJSON(path, data) {
+  return fetch(baseURL + path, {
+    body: JSON.stringify(data),
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    credentials: "include",
+    withCredentials: true
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      } else {
+        return res;
+      }
+    })
+    .then((res) => res.json());
+}
+
+async function getJSON(path) {
+  return fetch(baseURL + path,{
+    method: 'GET',
+    headers:{"accept": 'application/json'},
+    credentials: 'include',
+    withCredentials:true,
+})
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      } else {
+        return res;
+      }
+    })
+    .then((res) => res.json());
+}
